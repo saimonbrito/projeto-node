625b6494abc17e779bf830bd7a72fc1d51d8d67a
saimonBrito <britomauricio89@gmail.com>
2024-03-20 23:51:51 -0300

    first novo

---
+++ src/app.ts
@@ -0,0 +1,12 @@
+import fastify from 'fastify'
+import { knex } from './database'
+const app = fastify()
+
+app.get('/', async () => {
+  const tables = knex('sqlite_schema').select('*')
+  return tables
+})
+
+app.listen({ port: 3333 }).then(() => {
+  console.log('lets.. go watch')
+})
